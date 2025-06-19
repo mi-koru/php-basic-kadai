c308b39625bba3779d49a19c7367624383645730
mi-koru <mi1210t@icloud.com>
2025-06-19 20:21:42 +0900

    Add files via upload

---
+++ kadai_011/foreach.php
@@ -0,0 +1,17 @@
+<?php
+
+
+$begetable_datas = ['名前' => '玉ねぎ',
+                '値段' => 200,
+                '産地' => '北海道'
+                 ];
+
+
+foreach ($begetable_datas as $key => $value) {
+    echo "{$key} : {$value}<br>";
+       
+
+}
+
+?>
+            
